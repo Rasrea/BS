f7ba39563c8d63a4a88dd80aaab06b33c543a20d
BuildSight <13370510651@buildsight>
2026-06-16 11:48:24 +0800

    chore: bump to V1.0.3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -36,7 +36,7 @@
               <span class="w-1.5 h-1.5 rounded-full" :class="statusDotClass"></span>
               {{ statusText }}
             </span>
-            <span class="text-xs text-gray-400">V1.0.1</span>
+            <span class="text-xs text-gray-400">V1.0.3</span>
           </div>
         </div>
       </div>
